f3f47012b0957fdbbe09c1012a39fc7c311ba934
SatellaSoft <=>
2023-11-23 22:23:50 -0300

    feat: add paths and styles

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,16 @@
+import '../../shared/styles/styles.css'
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                        <a href="">New</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}

d267dacbf24eb4c9f78b8089d835fa465dac6b52
SatellaSoft <=>
2023-11-25 14:46:00 -0300

    feat: add types to Pokemon

--- src/types/Pokemon.ts
+++ src/types/Pokemon.ts
@@ -1,10 +1,13 @@
+export type Types = 'Fire' | 'Water' | 'Grass' | 'Electric' | 'Other'
+export type Colors = 'Red' | 'Blue' | 'Green' | 'Yellow' | 'Orange'
+
 export type Pokemon = {
     id: number;
     name: string;
-    type: string;
-    color: string;
+    type: Types
+    color: Colors
     image_url: string;
     level: string;
     description: string;
-    created_at: string;
+    created_at: Date;
 }
